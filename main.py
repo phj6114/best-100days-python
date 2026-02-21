8a80a8c7dcd385c9e08d7d48149654e246c4cb42
phj6114 <accessa14@gmail.com>
2026-02-21 18:44:17 +0900

    커피머신 초안 업로드

--- main.py
+++ main.py
@@ -3,15 +3,36 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 def coffe_machine():
-    menu = Menu()
 
-    user_chioce = input(f"What would you like?{menu.get_items()}\n")
+    while True:
+        menu = Menu()
+        coffee_maker = CoffeeMaker()
+        money_machine = MoneyMachine()
 
-    if menu.find_drink(user_chioce) is None:
-        return
+        # 주문
+        user_chioce = input(f"What would you like?{menu.get_items()}\n")
 
-    coffee_maker = CoffeeMaker()
-    coffee_maker.is_resource_sufficient(menu)
+        # 주문 메뉴 존재 확인
+        menu_item = menu.find_drink(user_chioce)
+        if menu_item is None:
+            continue
+
+        # 재료 잔량 확인
+        if not coffee_maker.is_resource_sufficient(menu_item):
+            continue
+
+        # 비용 안내
+        print(f"price is {menu_item.cost}")
+
+        # 비용 지불 - 금액 부족할 경우 break
+        if not money_machine.make_payment(menu_item.cost):
+            break
+
+        # 음료 제조
+        coffee_maker.make_coffee(menu_item)
+
+        # 리포트 출력
+        coffee_maker.report()
 
 if __name__ == '__main__':
     coffe_machine()
