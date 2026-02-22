38dc8ad9b32ceae6759585d3243f9aedc193dc99
phj6114 <accessa14@gmail.com>
2026-02-22 21:04:12 +0900

    124. 퀴즈 프로젝트 4부: 새로운 질문을 계속 보여주는 방법

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ question_bank = []
 for item in question_data:
     question_bank.append(Question(item["text"], item["answer"]))
 
-
+# call the quiz brain class and pass in the question bank
 quiz = QuizBrain(question_bank)
-answer = quiz.next_question()
+
+while quiz.still_has_questions():
+    answer = quiz.next_question()
+
 
 
