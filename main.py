0a87ab7c61720fabaa24083ff8631b0245a8c459
phj6114 <accessa14@gmail.com>
2026-02-22 20:51:06 +0900

    123. 퀴즈 프로젝트 3부: 퀴즈브레인과 next_question()

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for item in question_data:
     question_bank.append(Question(item["text"], item["answer"]))
 
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+answer = quiz.next_question()
+
+
